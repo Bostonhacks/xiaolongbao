6f7453495ff07c77fb767eee8e08a866eea9821d
Derek <derekxu888@gmail.com>
2025-03-21 16:49:42 -0400

    add get project route

---
+++ app/api/projects/[id]/route.ts
@@ -0,0 +1,36 @@
+
+
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+// GET /api/judging/judges
+
+export async function GET() {
+    try {
+        // get access_token cookie
+        const cookieStore = await cookies();
+        const accessToken = cookieStore.get('access_token');
+
+        // // fetch judges from the database
+        // const response = await fetch(`${process.env.API_URL}/judges`, {
+        //     headers: {
+        //         "Cookie": `access_token=${accessToken?.value || ""}`
+        //     }
+        // });
+
+        // if (!response.ok) {
+        //     return new Response(JSON.stringify(await response.json()), { status: 401 });
+        // }
+
+        // const judges = await response.json();
+
+        return NextResponse.json({
+            accessToken: accessToken
+        });
+
+
+    } catch (err) {
+        console.error('Failed to fetch judges:', err);
+        return NextResponse.json(err);
+    }
+}
